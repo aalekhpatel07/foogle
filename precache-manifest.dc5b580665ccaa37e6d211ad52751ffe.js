70df7b26be3f8a604cc333ea9cb92de2b0a0907c
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-04-06 06:10:51 -0500

    Updates

---
+++ precache-manifest.dc5b580665ccaa37e6d211ad52751ffe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4678cb30a34acff4b603e768d785a790",
+    "url": "/foogle/index.html"
+  },
+  {
+    "revision": "c0e32c4199c8db9467e7",
+    "url": "/foogle/static/css/main.1dc28576.chunk.css"
+  },
+  {
+    "revision": "07796a2d8bf642a92977",
+    "url": "/foogle/static/js/2.19d9d3d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foogle/static/js/2.19d9d3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e32c4199c8db9467e7",
+    "url": "/foogle/static/js/main.7ea755a2.chunk.js"
+  },
+  {
+    "revision": "2916e32cdcb045e65972",
+    "url": "/foogle/static/js/runtime-main.35059e07.js"
+  },
+  {
+    "revision": "f7fe1a38d99f33be4552b9b2a2a04e23",
+    "url": "/foogle/static/media/foogle.f7fe1a38.png"
+  }
+]);
