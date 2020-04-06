f717832be446dcee8f7c486a9c19422bc34165e9
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-04-06 06:41:01 -0500

    Logo click bug fix and background

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ const SearchBar = ({id, parentCallBack}) => {
 
     return (
     <div className="form-container">
-      <a href="/" alt="Foogle"><img src={require('../foogle.png')} alt="Foogle" className="logo-foogle"/></a>
+      <a href="/foogle" alt="Foogle"><img src={require('../foogle.png')} alt="Foogle" className="logo-foogle"/></a>
       <form id={id} className="search-form" onSubmit={handleSubmit}>
         <input id={function(){return id + '-keyword';}()} type="text" className="search-bar" placeholder="Enter any dish here..." minLength="1" autoComplete="off"/>
         <button id={function(){return id + '-button';}()} type="submit" className="button-style-e">
